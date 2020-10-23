a5c96352b1f71c706da4d92c6ac6c9022d201eda
Abiso <3300538020@qq.com>
2020-10-23 22:50:40 +0800

    显示员工列表

--- src/com/hotel/controller/EmployeeController.java
+++ src/com/hotel/controller/EmployeeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.hotel.pojo.Employee;
@@ -36,20 +38,32 @@ public class EmployeeController {
 		return service.insert(employee);
 	}
 	
-	@ResponseBody
+
 	@RequestMapping(value = "/select/all")
-	public List<Employee> selectAll(int page, int limit){
+	@ResponseBody
+	public Object selectAll(int page, int limit){
 		
 		PageHelper.startPage(page, limit);
 		
 		List<Employee> list = service.selectAll();
+		PageInfo info = new PageInfo<>(list);
+		long total = info.getTotal();
+		JSONArray jsonArray=new JSONArray();
+		if(list!=null){
+            jsonArray = new JSONArray();
+            for(Employee t: list){
+                jsonArray.add(t);
+
+            }
+        }
+		JSONObject jsObj = new JSONObject();
+		jsObj.put("code","0");	//响应码：0
+		jsObj.put("msg","");	//消息：空
+		jsObj.put("count",total);	//总记录数
+		jsObj.put("data",jsonArray);	//列表数据
 		
-	
-	
 		
-	
-		
-		return list;
+		return jsObj;
 		
 		
 	}
